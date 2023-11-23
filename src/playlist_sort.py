449d11ce7ff393f7fe6838566a3618cdefa9fa7c
Spencer Thayer <me@spencerthayer.com>
2023-11-22 16:19:43 -0800

    (main)
    Change Date/Time: 2023-11-22 16:19:43
    
    Changes Summary:        1 | 61 added, 137 removed
    M	src/playlist_sort.py | Added: 61, Removed: 137

--- src/playlist_sort.py
+++ src/playlist_sort.py
@@ -1,166 +1,90 @@
 import json
 import pandas as pd
-import numpy as np
 import os
 import re
 
-# Ensure the directory exists
-os.makedirs(os.path.dirname('../data/'), exist_ok=True)
+def setup_directories():
+    """Ensure necessary directories exist and return the data directory path."""
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(script_dir, '..', 'data')
+    os.makedirs(data_dir, exist_ok=True)
+    return data_dir
 
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.realpath(__file__))
+def load_weights(data_dir):
+    """Load weights from JSON file."""
+    with open(os.path.join(data_dir, 'playlists.json'), 'r') as json_file:
+        data = json.load(json_file)
+    return data.get('weights', {})  # Return only the 'weights' sub-dictionary
 
-# Construct the path to the data directory
-data_dir = os.path.join(script_dir, '..', 'data')
 
-# Read the weights from the JSON file
-with open(os.path.join(data_dir, 'playlists.json'), 'r') as json_file:
-    weights = json.load(json_file)
-    
-# Extract the randomize value
-randomize = weights.pop('randomize', True)  # Default to True if not found
-
-# Define a regular expression pattern for Spotify playlist IDs
-# Spotify IDs are typically 22 alphanumeric characters
-playlist_id_pattern = re.compile(r'^[A-Za-z0-9]{22}$')
-
-# Remove all keys that match the Spotify playlist ID pattern
-playlist_weights = {k: v for k, v in weights.items() if not playlist_id_pattern.match(k)}
-
-# Debugging print statements
-print('Debugging: Loaded weights from JSON:', playlist_weights)
-# print('Debugging: Path to playlists.json:', os.path.join(data_dir, 'playlists.json'))
-
-# Adjust the PSO parameters based on weights
-num_particles = int(playlist_weights.pop('num_particles'))
-iterations = int(playlist_weights.pop('iterations'))
-
-# Circle of Fifths Compatibility Check Function
-def is_compatible_circle_of_fifths(key1, mode1, key2, mode2):
-    # Major keys are 0 steps away on the circle from themselves, minor keys are 3 steps away.
-    major_keys = [0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5]  # C, G, D, A, E, B, F♯, C♯, A♭, E♭, B♭, F
-    minor_keys = [(k + 3) % 12 for k in major_keys]      # A, E, B, F♯, C♯, G♯, D♯, A♯, F, C, G, D
-
-    key1_circle = major_keys[key1] if mode1 == 1 else minor_keys[key1]
-    key2_circle = major_keys[key2] if mode2 == 1 else minor_keys[key2]
-
-    # Check if keys are the same or adjacent in the Circle of Fifths
-    return key1_circle == key2_circle or abs(key1_circle - key2_circle) == 1
-    print(f"Distance: {key1_circle}")
-
-# Function to calculate the distance between two keys in the Circle of Fifths
 def circle_of_fifths_distance(key1, mode1, key2, mode2):
+    """Calculate the Circle of Fifths distance between two keys."""
     major_keys = [0, 7, 2, 9, 4, 11, 6, 1, 8, 3, 10, 5]
     minor_keys = [(k + 3) % 12 for k in major_keys]
-
     key1_circle = major_keys[key1] if mode1 == 1 else minor_keys[key1]
     key2_circle = major_keys[key2] if mode2 == 1 else minor_keys[key2]
+    return min(abs(key1_circle - key2_circle), 12 - abs(key1_circle - key2_circle))
 
-    # Calculate the minimum circular distance
-    distance = min(abs(key1_circle - key2_circle), 12 - abs(key1_circle - key2_circle))
-    return distance
-    print(f"Distance: {distance}")
+def calculate_sort_score(row, df, weights):
+    score = 0
+    for feature in ['energy', 'tempo', 'key', 'mode']:
+        weight = weights.get(feature, 0)  # Use get method with default 0
+        if weight > 0:  # Process only if weight is greater than 0
+            if feature in ['key', 'mode']:
+                row_key = int(row['key']) if not pd.isna(row['key']) else 0
+                row_mode = int(row['mode']) if not pd.isna(row['mode']) else 0
+                differences = df.apply(lambda x: circle_of_fifths_distance(row_key, row_mode, int(x['key']), int(x['mode'])), axis=1)
+            else:
+                differences = abs(df[feature] - row[feature])
+            feature_score = weight * differences.sum()
+            score += feature_score
+    return score
 
-def calculate_fitness(particle, features_df, weightings):
-    # Initialize fitness
-    fitness = 0
+def assign_new_order(df):
+    """Assign a unique new order to each DataFrame row."""
+    df['new_order'] = range(len(df))
+    return df
 
-    # Iterate over each track in the particle's ordering
-    for i in range(len(particle) - 1):
-        current_track = features_df.iloc[particle[i]]
-        next_track = features_df.iloc[particle[i + 1]]
-
-        # Calculate the weighted difference for each feature
-        for feature, weight in weightings.items():
-            if feature in features_df.columns:
-                difference = abs(current_track[feature] - next_track[feature])
-                fitness += weight * difference
-
-    return -fitness  # Negative because we want to minimize the fitness
-    print(f"Fitness: -{fitness}")
-
-# # # # # Particle Swarm Optimization Algorithm
-def particle_swarm_optimization(features_df, weightings, num_particles, iterations):
-    num_tracks = features_df.shape[0]  # Number of tracks
-
-    # Initialize particle positions with random permutations of track indices
-    particle_positions = [np.random.permutation(num_tracks) for _ in range(num_particles)]
-
-    # Initialize particle velocities
-    particle_velocities = [np.zeros(num_tracks) for _ in range(num_particles)]
-
-    # Initialize best positions and fitness of particles
-    particle_best_positions = list(particle_positions)
-    particle_best_fitness = [calculate_fitness(p, features_df, weightings) for p in particle_best_positions]
-
-    # Determine global best
-    global_best_index = np.argmin(particle_best_fitness)
-    global_best_position = particle_best_positions[global_best_index]
-    global_best_fitness = particle_best_fitness[global_best_index]
-
-    # PSO parameters for inertia, cognitive, and social components
-    w_start, w_end = 0.9, 0.4  # Inertia weight start and end
-    c1, c2 = 2.0, 2.0  # Cognitive and social components
-
-    # Main loop of PSO
-    for iteration in range(iterations):
-        # Linearly decreasing inertia weight
-        w = w_start - ((w_start - w_end) * iteration / iterations)
-
-        for i in range(num_particles):
-            r1, r2 = np.random.rand(2)  # Random coefficients
-
-            # Update velocities
-            particle_velocities[i] = (w * particle_velocities[i] +
-                                      c1 * r1 * (particle_best_positions[i] - particle_positions[i]) +
-                                      c2 * r2 * (global_best_position - particle_positions[i]))
-
-            # Apply the velocity changes and maintain the indices within the range
-            particle_positions[i] = (particle_positions[i] + particle_velocities[i].astype(int)) % num_tracks
-            particle_positions[i] = np.argsort(particle_positions[i])  # Keep the order within the track index range
-
-            # Evaluate current fitness
-            current_fitness = calculate_fitness(particle_positions[i], features_df, weightings)
-
-            # Update best position and fitness for each particle
-            if current_fitness < particle_best_fitness[i]:
-                particle_best_positions[i] = particle_positions[i]
-                particle_best_fitness[i] = current_fitness
-
-            # Update global best
-            if current_fitness < global_best_fitness:
-                global_best_position = particle_positions[i]
-                global_best_fitness = current_fitness
-
-    return global_best_position
-# # # # # End of Particle Swarm Optimization Algorithm
-
-# The main function is where we integrate everything together. It will load the data, run the Particle Swarm Optimization (PSO) algorithm, and then output the sorted playlist.
 def main():
-    # Load the playlist features from the CSV file
+    data_dir = setup_directories()
+    playlist_weights = load_weights(data_dir)
+
     input_file = os.path.join(data_dir, 'playlist_features.csv')
-    features_df = pd.read_csv(input_file)
 
-    # Ensure 'key' is treated as numeric
+    try:
+        features_df = pd.read_csv(input_file)
+    except FileNotFoundError:
+        print(f"Error: File {input_file} not found.")
+        return
+
+    # Convert 'key' and 'mode' to numeric values, handling errors
     features_df['key'] = pd.to_numeric(features_df['key'], errors='coerce')
+    features_df['mode'] = pd.to_numeric(features_df['mode'], errors='coerce')
 
-    # Define the PSO parameters
-    num_particles = int(playlist_weights.pop('num_particles', 50))  # Default to 50 if not found
-    iterations = int(playlist_weights.pop('iterations', 2000))     # Default to 2000 if not found
+    # Additional debugging: Checking data types and value ranges
+    print("Data types:\n", features_df.dtypes)
+    print("Value ranges:\n", features_df.describe())
 
-    # Run the PSO algorithm with the weighted features
-    best_order = particle_swarm_optimization(features_df, playlist_weights, num_particles, iterations)
+    # Calculate sort scores
+    features_df['sort_score'] = features_df.apply(lambda row: calculate_sort_score(row, features_df, playlist_weights), axis=1)
 
-    # Apply the sorted order to the DataFrame
-    sorted_playlist = features_df.iloc[best_order].reset_index(drop=True)
+    # Debugging: Print sample sort scores
+    print("Sample calculated sort scores:\n", features_df['sort_score'].head())
 
-    # Create 'new_order' column corresponding to the sorted order
-    sorted_playlist['new_order'] = range(1, len(sorted_playlist) + 1)
+    # Sorting the playlist based on the calculated scores
+    sorted_playlist = features_df.sort_values(by='sort_score', ascending=True).reset_index(drop=True)
 
-    # Save the sorted playlist to a new CSV file
+    # Assigning a new order to the sorted playlist
+    sorted_playlist = assign_new_order(sorted_playlist)
+
+    # Debugging: Print DataFrame after sorting and the final sorted playlist
+    print("DataFrame before sorting:\n", features_df.head())
+    print("DataFrame after sorting:\n", sorted_playlist.head())
+    print("Final sorted playlist with new order:\n", sorted_playlist.head())
+
+    # Saving the sorted playlist to a CSV file
     output_file = os.path.join(data_dir, 'playlist_sorted.csv')
     sorted_playlist.to_csv(output_file, index=False)
-
     print(f'Playlist sorted and saved to {output_file}')
 
 if __name__ == '__main__':
